71a5d4e55f4f861a1358195ed59d0db17eeb6035
pucerpocok <schmidtzsolti91@gmail.com>
2018-11-19 12:39:24 +0100

    forgot to add aquarium lock scripts

---
+++ home_dir/Scripts/lock2.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#scrot /tmp/lockpic.png
+
+#convert /tmp/lockpic.png -blur 0x8 /tmp/lockpic.png
+#mogrify -scale 10% -scale 1000% /tmp/lockpic.png
+
+#composite /home/zschmidt/.config/logo -gravity center /tmp/lockpic.png /tmp/lockpic.png
+
+#i3lock -i /tmp/lockpic.png
+
+pkill -u "$USER" -USR1 dunst
+
+termite --title=own_screensaver -e "bash -c \"~/Scripts/start_aquarium.sh\"" &
+sleep 1
+#i3-msg -t get_tree > /tmp/ooooooo.js
+#i3-msg [title="own_screensaver"] fullscreen
+i3-msg [title="own_screensaver"] fullscreen global
+xtrlock
+i3-msg [title="own_screensaver"] kill
+
+pkill -u "$USER" -USR2 dunst
