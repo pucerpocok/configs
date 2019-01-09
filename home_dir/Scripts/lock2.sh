22ad024103744e69f5e055faee05b367930e6685
pucerpocok <schmidtzsolti91@gmail.com>
2019-01-09 10:17:00 +0100

    wrong parameters were used

--- home_dir/Scripts/lock2.sh
+++ home_dir/Scripts/lock2.sh
@@ -9,7 +9,7 @@
 
 #i3lock -i /tmp/lockpic.png
 
-sh ~/Scripts/notif.sh --off
+sh ~/Scripts/notif.sh --disable
 
 termite --title=own_screensaver -e "bash -c \"~/Scripts/start_aquarium.sh\"" &
 sleep 1
@@ -19,4 +19,4 @@ i3-msg [title="own_screensaver"] fullscreen global
 xtrlock
 i3-msg [title="own_screensaver"] kill
 
-sh ~/Scripts/notif.sh --on
+sh ~/Scripts/notif.sh --enable
